0434847752816d06041d0cb9faf78c52b31ed0b3
syhan <syhannnn@gmail.com>
2017-06-06 15:09:05 +0800

    Tree exercise in Chapter 3

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,51 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](tree: Tree[A]): (Int, Int) = {
+    def go(t: Tree[A], acc: (Int, Int)): (Int, Int) = {
+      t match {
+        case Leaf(_) => (acc._1 + 1, acc._2)
+        case Branch(l, r) => {
+          val (x1, y1) = go(l, acc)
+          val (x2, y2) = go(r, acc)
 
+          (x1 + x2, y1 + y2 + 1)
+        }
+      }
+    }
+
+    go(tree, (0, 0))
+  }
+
+  def maximum(tree: Tree[Int]): Int = {
+    def go(t: Tree[Int], m: Int): Int = {
+      t match {
+        case Leaf(v) => m max v
+        case Branch(l, r) => go(l, m) max go(r, m)
+      }
+    }
+
+    go(tree, Int.MinValue)
+  }
+
+  def depth[A](tree: Tree[A]): Int = {
+    def go(t: Tree[A], d: Int): Int = {
+      t match {
+        case Leaf(_) => d + 1
+        case Branch(l, r) => go(l, d) max go(r, d)
+      }
+
+    }
+
+    go(tree, 0)
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+    tree match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+  }
 
 
 }
